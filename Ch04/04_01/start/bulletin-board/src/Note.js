1471bc47d077a04f02a1008efc1a1459e2e55173
Simon Kongsberg <simon_ceese@hotmail.com>
2018-05-01 16:22:10 +0200

    Completed Chapter 4 and 5, is on Chapter 6_03

--- Ch04/04_01/start/bulletin-board/src/Note.js
+++ Ch04/04_01/start/bulletin-board/src/Note.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
-import FaFloppy0 from 'react-icons/lib/fa/floppy0'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
     constructor(props) {
@@ -11,38 +11,60 @@ class Note extends Component {
         }
         this.edit = this.edit.bind(this)
         this.remove = this.remove.bind(this)
+        this.save = this.save.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
     }
     edit(){
         this.setState({
             editing: true
         })
     }
+
     remove(){
-        alert('removing note')
+        this.props.onRemove(this.props.index)
     }
 
+    save(e) {
+        e.preventDefault()
+        this.props.onChange(this._newText.value, this.props.index)
+        this.setState({
+            editing: false
+        })
+    }
     renderForm() {
         return(
             <div className="note">
-                <form>
-                    <textarea />
-                    <button ><FaFloppy0 /></button>
+                <form onSubmit={this.save}>
+                    <textarea ref={input => this._newText = input }/>
+                    <button><FaFloppyO /></button>
                 </form>
             </div>
         )
     }
 
-    render()
+    renderDisplay()
         {
         return (
             <div className="note">
-                <p>Learn React </p>
+                <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit} id="edit"><FaPencil /></button>
                     <button onClick={this.remove} id="remove"><FaTrash /></button>            
                 </span>
             </div>
         )
+    }
+    render() {
+        /* Optional refactor, shorter, easier to read:
+        return this.state.editing ? this.renderForm() : this.renderDisplay()
+        */
+
+        if(this.state.editing) {
+            return this.renderForm()
+        } else {
+            return this.renderDisplay()
+        }
     }
 }
 
